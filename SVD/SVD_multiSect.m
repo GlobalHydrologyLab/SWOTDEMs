67abf2d6f34c677bb4f1f023b03e815e9b0b270f
Ted <Ted@oasis-maclabs00193.dhcp.unc.edu>
2018-01-10 14:48:51 -0500

    Added cumulative % option for singular value picking. Removed far-range data from pass 527.

--- SVD/SVD_multiSect.m
+++ SVD/SVD_multiSect.m
@@ -16,13 +16,21 @@ close all
 %Sac
 load('/Users/Ted/Documents/MATLAB/SWOTDEMs/Sacramento/transformedSacDataV2.mat')
 zField = 'geoHeight';
+
+% hard-coded removal of far range data from pass 527
+for i = 10:17
+    simulated(i).geoHeight(339:487) = NaN;
+end
+% % hard-coded removal of high mean z days for sacramento
+simulated(16) = [];
+truth(16)=[];
+simulated(6) = [];
+truth(6)=[];
+
+
 clearvars -except simulated truth zField
 
-% % hard-coded removal of high mean z days for sacramento
-% simulated(16) = [];
-% truth(16)=[];
-% simulated(6) = [];
-% truth(6)=[];
+
 
 %Group data into matrices without gaps, intelligently deleting data so that
 %all sections have >= sectMin rows
@@ -63,6 +71,7 @@ for r = min(section):max(section)
     zresid = reshape(z-zhat, [], nProf);
     
     [U,S,V] = svd(zresid,0);
+    
     %----------------------------------------------------------------------
     % Determine best rank 
     %
@@ -70,17 +79,21 @@ for r = min(section):max(section)
     % approach perform better than the other in some cases. Maybe some kind
     % of consensus with a third method could work.
     %----------------------------------------------------------------------
-
     S = diag(S); %extract diagonal values
 
     % WORKING IDEAS:
-
+    
     %largest decrease in magnitude
-%     [~,iSV] = min(diff(S));
+%     [~,iSV] = min(diff(S))
 
     %inter quartile range approach.
     IQR = iqr(S);
-    iSV = find(S >= median(S) + 1.5*IQR,1,'last');
+    iSV = find(S >= median(S) + 2*IQR,1,'last');
+    
+    %cumulative percent of sing. val. threshold.
+%     p = 0.4;
+%     iSV = find(cumsum(S)./sum(S) >= p,1,'first')
+    
 
     S = diag(S); %recreate diagonal matrix
     %----------------------------------------------------------------------
@@ -116,6 +129,9 @@ if hasReaches
             fitSim = polyfit(sAll(inReach,p),zAll(inReach,p),1);
             fitSVD = polyfit(sAll(inReach,p),z2All(inReach,p),1);
 
+            %calc reach length
+            RL(r,1) = range(sAll(inReach,1));
+            
             inReach = truth(p).reach == r;
             fitTruth = polyfit(truth(p).sCoord(inReach), ... 
                 truth(p).(zField)(inReach),1);
@@ -128,8 +144,6 @@ if hasReaches
     %         truthSlope(r,p) = fitTruth(1);
         end
 
-        %calc reach length
-        RL(r,1) = range(sAll(inReach,1));
 
     end
 end
@@ -159,6 +173,13 @@ xlabel('Flow Distance (km)')
 ylabel('Elevation (m)')
 title('Original Simulated Profiles')
 
+for r = reaches
+    ir = find(simulated(1).reach == r,1,'last');
+    xr = simulated(1).sCoord(ir);
+    yr = truth(3).(zField)(ir);
+    plot([xr xr]./1000,[yr yr], 'r*','LineWidth',2)
+end
+
 subplot(2,1,2);
 plot(skm,z2All)
 hold on
@@ -171,6 +192,12 @@ allAxes = findobj(handle, 'type', 'axes');
 linkaxes(allAxes);
 set(gcf,'Units','normalized','Position', [0.2, 0.1, 0.6, 0.9])
 
+for r = reaches
+    ir = find(simulated(1).reach == r,1,'last');
+    xr = simulated(1).sCoord(ir);
+    yr = truth(3).(zField)(ir);
+    plot([xr xr]./1000,[yr yr], 'r*','LineWidth',2)
+end
 
 %singular values
 figure()
@@ -194,14 +221,14 @@ z2Err = z2All - truthZ;
 
 
 %node errors
-% RMSE = sqrt(nanmean(zErr.^2,1));
-% RMSESVD = sqrt(nanmean(z2Err.^2,1));
-MAE = nanmean(abs(zErr));
-MAESVD = nanmean(abs(z2Err));
+RMSE = sqrt(nanmean(zErr.^2,1));
+RMSESVD = sqrt(nanmean(z2Err.^2,1));
+MAE = nanmean(abs(zErr),1);
+MAESVD = nanmean(abs(z2Err),1);
 figure()
-% bar([RMSE' RMSESVD'],1,'grouped')
-bar([MAE' MAESVD'],1,'grouped')
-ylabel('MAE (m)')
+bar([RMSE' RMSESVD'],1,'grouped')
+% bar([MAE' MAESVD'],1,'grouped')
+ylabel('RMSE (m)')
 xlabel('Profile Number')
 title('Node-level height errors')
 legend('Original Data','Low Rank','Location','Northwest')
@@ -210,24 +237,23 @@ colormap(c([10,40],:))
 
 
 % reach slope errors
-    simSlopeMAE = nanmean(abs(simSlopeErr),1);
-    SVDSlopeMAE = nanmean(abs(SVDSlopeErr),1);
-    simSlopeRMSE = sqrt(nanmean(simSlopeErr.^2,1));
-    SVDSlopeRMSE = sqrt(nanmean(SVDSlopeErr.^2,1));
-    figure()
-    bar([simSlopeMAE' SVDSlopeMAE'] .* 10^5,1,'grouped')
-%     bar([simSlopeRMSE' SVDSlopeRMSE'] .* 10^5,1,'grouped')
-    ylabel('MAE (cm/km)')
-    xlabel('Profile Number')
-    title('Reach-level slope errors')
-    legend('Original Data','Low Rank','Location','Northwest')
+simSlopeMAE = nanmean(abs(simSlopeErr),1);
+SVDSlopeMAE = nanmean(abs(SVDSlopeErr),1);
+simSlopeRMSE = sqrt(nanmean(simSlopeErr.^2,1));
+SVDSlopeRMSE = sqrt(nanmean(SVDSlopeErr.^2,1));
+figure()
+%     bar([simSlopeMAE' SVDSlopeMAE'] .* 10^5,1,'grouped')
+bar([simSlopeRMSE' SVDSlopeRMSE'] .* 10^5,1,'grouped')
+ylabel('RMSE (cm/km)')
+xlabel('Profile Number')
+title('Reach-level slope errors')
+legend('Original Data','Low Rank','Location','Northwest')
 %     c = lines;
 %     colormap(c(1:2,:))
-    c = gray;
-    colormap(c([10,40],:))
+c = gray;
+colormap(c([10,40],:))
 
 % % R-mode analysis
-
 % R = V*S2;
 % figure()
 % plot(R(:,1),R(:,2),'r.','MarkerSize',24)

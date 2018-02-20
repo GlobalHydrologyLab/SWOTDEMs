d1849be94a11f2b5401b4b6fc22034af1349c7c5
Ted <Ted@oasis-maclabs00193.dhcp.unc.edu>
2018-02-20 14:06:09 -0500

    Added option for operating on residuals or not.

--- SVD/SVD_multiSect_varReach.m
+++ SVD/SVD_multiSect_varReach.m
@@ -13,7 +13,7 @@
 %
 % - make sure # of singular values chosen is providing best results.
 %
-% - merge residual and noResid versions using switch.
+% - merge resid and noResid versions using switch.
 %
 %--------------------------------------------------------------------------
 
@@ -21,6 +21,8 @@ clear
 close all
 
 targetRLkm = 10;
+sectMin = 25;
+rmResidOpt = 1;
 
 % river = 'Sac';
 % river = 'Po';
@@ -65,13 +67,12 @@ switch river
 end
 
 
-clearvars -except simulated truth zField targetRLkm
+clearvars -except simulated truth zField targetRLkm sectMin rmResidOpt
 
 
 %Group data into matrices without gaps, intelligently deleting data so that
 %all sections have >= sectMin rows
 nProf = length(simulated);
-sectMin = 25;
 simAllign = nodeAllign(simulated);
 
 nodeRng = [min(simAllign.node(1,:)), max(simAllign.node(end,:))];
@@ -100,7 +101,8 @@ for r = min(section):max(section)
     truthReach = trimFields(truthAllign,inSect);
     
     %assemble full rectangular matrices of s,z data 
-    z = zArray(inSect,:);
+%     z = zArray(inSect,:);
+    z = truthAllign.(zField)(inSect,:);
     [z, delCol] = nanRows(z,2);
     s = simAllign.sCoord(inSect,~delCol);
 
@@ -110,25 +112,25 @@ for r = min(section):max(section)
         s(mMiss(i),nMiss(i)) = nanmean(s(mMiss(i),:));
     end
     
-    % fit polynomial to all data in reach
-    nProfSect = size(z,2);
-    polyOrder = 1;
-    sv = reshape(s,[],1);
-    zv = reshape(z,[],1);
-    [m,~,mu] = polyfit(sv,zv,polyOrder);
-    
-    %SVD on residuals
-    zhat = polyval(m,s,[],mu);
-    zresid = reshape(z-zhat, [], nProfSect);
-    
-    [U,S,V] = svd(zresid);
+    if rmResidOpt
+        % fit polynomial to all data in reach
+        nProfSect = size(z,2);
+        polyOrder = 1;
+        sv = reshape(s,[],1);
+        zv = reshape(z,[],1);
+        [m,~,mu] = polyfit(sv,zv,polyOrder);
+
+        %SVD on residuals
+        zhat = polyval(m,s,[],mu);
+        zresid = reshape(z-zhat, [], nProfSect);
+
+        [U,S,V] = svd(zresid);
+    else
+        [U,S,V] = svd(z);
+    end
     
     %----------------------------------------------------------------------
     % Determine best rank 
-    %
-    % This section needs to be finished- both break in magnitude and IQR
-    % approach perform better than the other in some cases. Maybe some kind
-    % of consensus with a third method could work.
     %----------------------------------------------------------------------
     SVal = diag(S);
     
@@ -142,14 +144,14 @@ for r = min(section):max(section)
     try
         iSV(r) = find(SVal >= median(SVal) + 2*IQR,1,'last');
     catch
-        iSV(r) = 1;
+        iSV(r) = 1; %catch 0 evaluation
     end
     
     %cumulative percent of sing. val. threshold.
-%     p = 0.8;
-%     iSV(r) = find(cumsum(SVal)./sum(SVal) >= p,1,'first');
+    p = 0.99;
+    iSV(r) = find(cumsum(SVal)./sum(SVal) >= p,1,'first');
 
-    iSV(r) = 2;
+%     iSV(r) = 2;
     %----------------------------------------------------------------------
 
     %now modify S, removing smaller components.
@@ -157,11 +159,13 @@ for r = min(section):max(section)
     S2(iSV+1:end,:) = 0;
     
     %recombine
-    z2resid = U*S2*V';
-    z2 = z2resid + polyval(m,s,[],mu);
+    if rmResidOpt
+        z2All(inSect,~delCol) = U*S2*V' + zhat;
+    else
+        z2All(inSect,~delCol) = U*S2*V';
+    end
   
-    % join section data for later comparison
-    z2All(inSect,~delCol) = z2;
+    % join section data for later comparison   
     sAll(inSect,~delCol) = s;
 end
 
@@ -203,9 +207,9 @@ for r = reaches
             fitTruth = polyfit(truthAllign.sCoord(inReach,p), ... 
                 truthAllign.(zField)(inReach,p),1);
             
-            simStats.slope(r,p) = fitSim(1) .*100000;
-            svdStats.slope(r,p) = fitSVD(1) .*100000;
-            truthSlope(r,p) = fitTruth(1) .*100000;
+            simStats.slope(r,p) = fitSim(1) .* 100000; %m/m to cm/km
+            svdStats.slope(r,p) = fitSVD(1) .* 100000;
+            truthSlope(r,p) = fitTruth(1) .* 100000;
 
             simStats.slopeErr(r,p) = truthSlope(r,p) - simStats.slope(r,p);
             svdStats.slopeErr(r,p) = truthSlope(r,p) - svdStats.slope(r,p);
@@ -230,13 +234,21 @@ svdStats.reachZErr = reachAvgTruth - svdStats.reachAvgZ;
 %--------------------------------------------------------------------------
 
 %rothko section plot
+% figure()
+% imagesc(observedBy .* section)
+% xlabel('Profile')
+% ylabel('Node')
+% c = lines;
+% c = c(1:max(section),:);
+% colormap([1 1 1; c])
+
+%coverage/elevation plot
 figure()
-imagesc(observedBy .* section)
-xlabel('Profile')
-ylabel('Node')
-c = lines;
-c = c(1:max(section),:);
-colormap([1 1 1; c])
+imAlpha=ones(size(zAll));
+imAlpha(isnan(zAll))=0;
+imagesc(zAll,'AlphaData',imAlpha);
+colormap(brewermap(64,'YlGnBu'))
+set(gca,'color',0*[1 1 1]);
 
 %singular values
 figure()
@@ -244,11 +256,13 @@ bar(diag(S))
 xlabel('Singular Value Number')
 ylabel('Singular Value Magnitude')
 title('Singular Values of Elevation Residuals')
+if ~rmResidOpt; set(gca,'YScale','log'); end
 
 %original and approx profiles
 handle = figure();
 subplot(2,1,1);
-plot(skm,zArray)
+% plot(skm,zArray)
+plot(skm,truthAllign.(zField))
 hold on
 plot(skm,truthAllign.(zField)(:,3),'k','Linewidth',2)
 xlabel('Flow Distance (km)')
@@ -276,8 +290,6 @@ scatter(xr,yr, 100, reaches,'filled')
 allAxes = findobj(handle, 'type', 'axes');
 linkaxes(allAxes);
 set(gcf,'Units','normalized','Position',[0.013672 0.013194 0.59922 0.91528])
-
-
 
 % %epdf of all node errors
 % figure()
@@ -324,12 +336,12 @@ hold on
 ksdensity(svdStats.slopeErr(:))
 legend('original','LRA')
 
-subplot(2,2,4)
-scatter(simStats.slopeRRMSE, svdStats.slopeRRMSE,[],nCol,'filled')
-scatter1to1(gca,'origin');
-xlabel('Simulated RRMSE(%)')
-ylabel('LRA RRMSE()')
-title('RRMSE')
+% subplot(2,2,4)
+% scatter(simStats.slopeRRMSE, svdStats.slopeRRMSE,[],nCol,'filled')
+% scatter1to1(gca,'origin');
+% xlabel('Simulated RRMSE(%)')
+% ylabel('LRA RRMSE()')
+% title('RRMSE')
 
 set(gcf,'Units','Normalized','Position',[0.58008 0.21389 0.37461 0.55833])
 
